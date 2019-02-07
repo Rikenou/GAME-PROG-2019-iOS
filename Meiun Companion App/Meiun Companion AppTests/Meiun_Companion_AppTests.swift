6ae12958d5e4ebcec3c76a255ca5002999303d6c
Jon <forfakethings@hotmail.com>
2019-02-07 16:36:18 -0500

    Make thing

---
+++ Meiun Companion App/Meiun Companion AppTests/Meiun_Companion_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meiun_Companion_AppTests.swift
+//  Meiun Companion AppTests
+//
+//  Created by Jonathan Leung on 2/7/19.
+//  Copyright © 2019 Jonathan Leung. All rights reserved.
+//
+
+import XCTest
+@testable import Meiun_Companion_App
+
+class Meiun_Companion_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
