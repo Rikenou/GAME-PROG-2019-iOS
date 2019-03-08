58bfca412f77afb67513fcb7c5ae369b94cf7b99
Jon <EXOLEGENDS@hotmail.com>
2019-03-08 17:51:17 -0500

    Restarted project with CocoaPod support

---
+++ Meiun Journey App/Meiun Journey AppTests/Meiun_Journey_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meiun_Journey_AppTests.swift
+//  Meiun Journey AppTests
+//
+//  Created by Jonathan Leung on 3/8/19.
+//  Copyright © 2019 Jonathan Leung. All rights reserved.
+//
+
+import XCTest
+@testable import Meiun_Journey_App
+
+class Meiun_Journey_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
