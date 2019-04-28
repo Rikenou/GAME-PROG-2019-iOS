12a5ce947c56fefce854ed65f86d3b348f5eeffe
Jonathan Leung <>
2019-04-28 04:58:12 -0400

    Added animations Added Jump mechanics Added simple physics

--- Meiun Journey App/Meiun Journey App/Platform.swift
+++ Meiun Journey App/Meiun Journey App/Platform.swift
@@ -11,7 +11,16 @@ import GameplayKit
 
 class Platform: minigameScene {
     
+    var fingerOff = true;
+    
     var quitTextBox : SKSpriteNode!;
+    var Black : SKSpriteNode!;
+    
+    var blackTexture: [SKTexture]!;
+    
+    var onGround = true;
+    var groundPos : CGPoint!;
+    var blackSpeedY = CGFloat(0.0);
     
     override func sceneDidLoad() {
         
@@ -37,6 +46,24 @@ class Platform: minigameScene {
         addChild(quitTextBox);
         addChild(quitText);
         
+        var BlackImageName = "BlackIdleFrame";
+        
+        Black = SKSpriteNode.init(imageNamed: BlackImageName + String(0));
+        Black.position = CGPoint(x: displaySize.width * 0.8, y: displaySize.height * 0.3);
+        groundPos = Black.position;
+        Black.size = CGSize(width: Black.size.width * 0.75, height: Black.size.height * 0.75);
+        blackTexture = [SKTexture]();
+        
+        for i in 0 ..< 9 {
+            
+            blackTexture.append(SKTexture(imageNamed: BlackImageName + String(i)));
+            
+        }
+        
+        blackTexture.append(SKTexture(imageNamed: "JumpFrame0"));
+        
+        addChild(Black);
+        
     }
     
     
@@ -47,6 +74,13 @@ class Platform: minigameScene {
             ViewController?.returnHome();
             
         }
+            
+        else{
+            
+            fingerOff = false;
+            blackSpeedY = 100.0;
+            
+        }
         
     }
     
@@ -54,11 +88,96 @@ class Platform: minigameScene {
     }
     
     override func touchUp(atPoint pos : CGPoint) {
+        
+        fingerOff = true;
+        
     }
     
-    
+    var timer = CGFloat(0.0);
+    var lastTime : TimeInterval!;
+    var blackImageFrame = 0;
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if(lastTime == nil){
+            
+            lastTime = currentTime;
+            
+        }
+        
+        var deltaTime = CGFloat(currentTime - lastTime);
+        lastTime = currentTime;
+        
+        var downForce = CGFloat(0.0);
+        
+        if(fingerOff && blackSpeedY > 0){
+            
+            downForce = CGFloat(15.0 * -9.8);
+            
+        }
+        
+        var s = blackSpeedY * deltaTime;
+        s += 0.5 * (-9.8 * 5.0 + downForce) * deltaTime * deltaTime;
+        
+        Black.position.y += s;
+        
+        blackSpeedY += 0.5 * (-9.8 * 5.0 + downForce) * deltaTime;
+        
+        if(Black.position.y > groundPos.y){
+            
+            onGround = false;
+            
+        }
+        else{
+            
+            if(onGround == false){
+                
+                blackImageFrame = 0;
+                
+            }
+            
+            if(Black.position.y < groundPos.y){
+                
+                Black.position.y = groundPos.y;
+                
+            }
+            
+            onGround = true;
+            
+        }
+        
+        if(onGround == true){
+        
+            if(timer < 0.1){
+            
+                timer += deltaTime;
+            
+            }
+        
+            else{
+        
+                if(blackImageFrame < 8){
+            
+                    blackImageFrame += 1;
+            
+                }
+        
+                else{
+                    blackImageFrame = 0;
+                }
+        
+                Black.texture = blackTexture[blackImageFrame];
+                timer = 0.0;
+            
+            }
+            
+        }
+        
+        else{
+            
+            Black.texture = blackTexture[9];
+            
+        }
         
     }
     
